7f5f0d7f0eac759760c5c3d1598accd3b4122bdc
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-04-27 15:47:54 +0300

    Add 2nd batch of Posts.

--- new_zealand_spider.py
+++ new_zealand_spider.py
@@ -13,7 +13,7 @@ class AllSpider(scrapy.Spider):
 
     custom_settings = {
         'ROBOTSTXT_OBEY': 0,
-        'FEED_URI': 'gpnz_test_batch.xml',
+        'FEED_URI': 'gpnz_test_batch_2.xml',
         'FEED_FORMAT': 'xml',
         'FEED_EXPORT_ENCODING': 'utf-8',
     }
@@ -21,21 +21,70 @@ class AllSpider(scrapy.Spider):
     def start_requests(self):
 
         start_urls = {
-            #Story
+            # Story
+            # - 1st batch
             'http://www.greenpeace.org/new-zealand/en/blog/life-aboard-the-stop-deep-sea-oil-flotilla/blog/34474/':('Story','Resist','Oil&Gas', '', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/blog/fonterra-embarrasses-the-government-over-palm/blog/33265/':('Story','Protect','Forests', '', 'Migrate'),
-            'http://www.greenpeace.org/new-zealand/en/blog/petition-sign-the-deep-water-oil-drilling-in-/blog/24877/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/petition-sign-the-deep-water-oil-drilling-in-/blog/24877/':('Story','Resist','Oil&Gas', 'Oceans', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/blog/remember-the-rainbow-warrior-and-the-marshall/blog/24895/':('Story','Greenpeace','AboutUs', '', 'Migrate'),
 
-            #Press Release
+            # - 2nd batch
+            'http://www.greenpeace.org/new-zealand/en/blog/tiama-joins-the-flotilla-to-stop-deep-sea-oil/blog/34473/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/iwi-fishing-boat-disrupts-oil-survey-ship/blog/34408/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/stopping-deep-sea-oil-vanessas-log-12042011/blog/34252/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/stopping-deep-sea-oil-vanessas-log/blog/34249/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/vanessas-blog-seismic-events-at-sea/blog/34156/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/jefs-blog-toward-whangaparoa/blog/34155/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/fighting-spirit-at-flotilla-send-off/blog/33965/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/brownlee-and-oil-relics-of-a-dying-age/blog/24866/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/are-the-governments-deep-water-oil-plans-runn/blog/35784/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/police-protect-us-oil-giant-anadarkos-survey-/blog/37357/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/rena-oil-spill-an-unfortunate-lesson/blog/37226/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/the-oil-is-less-obvious-but-the-problem-is-sp/blog/37351/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/rena-oil-spill-could-make-deep-sea-oil-drilli/blog/37303/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/blog/toxic-rena-oil-washes-ashore/blog/37291/':('Story','Resist','Oil&Gas', '', 'Migrate'),
+
+            # Press Release
+            # - 1st batch
             'http://www.greenpeace.org/new-zealand/en/press/John-Key-Should-Resign-as-Tourism-Minister-Greenpeace/':('Press Release','Protect','Freshwater', 'Climate', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/press/Backdoor-talks-to-keep-coal-powering-NZ-an-absolute-disgrace/':('Press Release','Resist','Coal', 'Climate', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/press/Greenpeace-Response-to-TPP-Protests/':('Press Release','Resist','Freedom', '', 'Migrate'),
 
-            #Publication
+            # - 2nd batch
+            'http://www.greenpeace.org/new-zealand/en/press/Record-shattering-NASA-announcement-indicates-NZ-faces-extreme-weather-events/':('Press Release','Protect', 'Climate', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Greenpeace-activists-convicted-but-receive-no-further-punishment-over-10-hour-occupation-of-the-Tangaroa/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Governments-failing-oil-agenda-hits-hurdle-as-all-NZ-exploration-plans-are-trashed/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Genesis-letter/':('Press Release','Resist','Coal', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Greenpeace-launches-civil-disobedience-campaign-against-NZs-largest-oil-event/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Climate-action-a-matter-of-life-and-death-after-February-temperatures-smash-global-records/':('Press Release','Protect','Climate', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Landcorp-Ditches-Industrial-Dairy-Plans-Over-Pollution-Fears-Greenpeace-Response/':('Press Release','Protect','Freshwater', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Whiskas-Embroiled-in-Modern-Day-Slavery-Scandal/':('Press Release','Protect','Oceans', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Dairy-Irrigation-Subsidies-Must-Be-Ditched-to-Avert-Crisis-Greenpeace-Says/':('Press Release','Resist','Freedom', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Greenpeace-response-to-MRAG-Asia-Pacific-report-into-IUU-fishing-in-the-Pacific/':('Press Release','Protect','Oceans', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/US-environmental-kingpin-Bill-McKibben-throws-his-weight-behind-civil-disobedience-in-NZ/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Hundreds-descend-on-Central-Auckland-to-blockade-NZs-largest-oil-event/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/press/Oil-Block-Offer-a-Desperate-Last-Ditch-Bid-by-the-Government-to-Try-and-Save-Failing-Oil-Plans/':('Press Release','Resist','Oil&Gas', 'Climate', 'Migrate'),
+
+            # Publication
+            # - 1st batch
             'http://www.greenpeace.org/new-zealand/en/reports/Made-in-Taiwan/':('Publication','Protect','Oceans', '', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/reports/global-wind-energy-outlook/':('Publication','Transform','Renewables', '', 'Migrate'),
             'http://www.greenpeace.org/new-zealand/en/reports/OIA-Docs-for-Thompson-and-Clark-MBIE-Relationship/':('Publication','Resist','Freedom', 'Oil&Gas', 'Migrate'),
+
+            # - 2nd batch
+            'http://www.greenpeace.org/new-zealand/en/reports/future-investment-energy/':('Publication','Transform','Renewables', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/the-power-and-appeal-of-wind/':('Publication','Transform','Renewables', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/dirty-old-coal/':('Publication','Resist','Coal', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/enviro-impacts-of-coal/':('Publication','Resist','Coal', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Save-our-beaches-from-oil-disaster/':('Publication','Resist','Oil&Gas', 'Coal', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Changing-Tuna/':('Publication','Protect','Oceans', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Out-Of-Our-Depth-Deep-sea-oil-exploration-in-New-Zealand/':('Publication','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/The-Future-is-Here-report/':('Publication','Transform','Renewables', 'Oil&Gas', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Joint-Statement-on-Crown-Minerals-Bill-Amendment-2013/':('Publication','Resist','Oil&Gas', 'Freedom', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/-Response-to-Minister-of-Energy-and-Resources-July-2013/':('Publication','Resist','Oil&Gas', 'Freedom', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/New-Zealand-Oil-Spill-Report/':('Publication','Resist','Oil&Gas', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Fewer-boats-more-fish/':('Publication','Protect','Oceans', '', 'Migrate'),
+            'http://www.greenpeace.org/new-zealand/en/reports/Out-of-line---The-failure-of-the-global-tuna-longline-fisheries/':('Publication','Protect','Oceans', '', 'Migrate'),
         }
 
         for url,data in start_urls.iteritems():
