83d899d9355d4a34fd1f2212eddb891cbb04480b
elerey <lreyes@greenpeace.org>
2019-03-13 18:02:15 +0100

    Added links for second iteration to Thai spider

--- Thailand/settings.py
+++ Thailand/settings.py
@@ -90,4 +90,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 LOG_STDOUT = True
-LOG_FILE = 'output-gbth_staging_v1.txt'
+LOG_FILE = 'output-gbth_staging_v2.txt'
